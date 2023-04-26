01c729ed8f8f6a2707ea1d61d167513ca26f0a44
Desire <desireirankundawilliams@gmail.com>
2023-04-26 12:34:55 +0200

    added validation

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,35 +1,65 @@
 import { Container, Card, Stack, Button, Form } from "react-bootstrap";
 import image from "../image/contactus.jpg";
+import { useForm, ValidationError } from "@formspree/react";
 
 function FormPage() {
+  const [state, handleSubmit] = useForm("xyyalvwd");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
   return (
     <Container className="roomfac">
       <br />
       <Card style={{ width: "30rem" }}>
-        <Card.Img variant="top" src={image} alt="contact us" height={200} width={100} />
+        <Card.Img
+          variant="top"
+          src={image}
+          alt="contact us"
+          height={200}
+          width={100}
+        />
         <Card.Body>
-          <Card.Title style={{textAlign: "center"}} >Hello I'm <span style={{fontWeight: "bold"}}>Desire Irankunda</span></Card.Title>
-          <Form>
+          <Card.Title style={{ textAlign: "center" }}>
+            Hello I'm{" "}
+            <span style={{ fontWeight: "bold" }}>Desire Irankunda</span>
+          </Card.Title>
+          <form onSubmit={handleSubmit}>
             <Form.Group className="mb-3">
               <Form.Label>Full Names</Form.Label>
-              <Form.Control type="text" placeholder="Enter your full names" />
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Enter your full names"
+                required
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="name@example.com"
+                required
+              />
             </Form.Group>
             <Form.Group
               className="mb-3"
               controlId="exampleForm.ControlTextarea1"
             >
               <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
+              <Form.Control as="textarea" rows={3} name="message" required />
             </Form.Group>
 
             <Stack gap={2} className="col-md-5 mx-auto">
-              <Button variant="secondary">Submit form</Button>
+              <Button
+                variant="secondary"
+                type="submit"
+                disabled={state.submitting}
+              >
+                Submit form
+              </Button>
             </Stack>
-          </Form>
+          </form>
         </Card.Body>
       </Card>
     </Container>
